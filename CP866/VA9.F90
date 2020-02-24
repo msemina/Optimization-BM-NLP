847845ead1d83306455a137b94813b7ade6d8c2e
MahBoiDeveloper <mah.boi.developer@gmail.com>
2020-02-25 01:56:47 +0300

    Переписаны A9 и VA9 на Fortran 90

--- CP866/VA9.F90
+++ CP866/VA9.F90
@@ -1,79 +1,53 @@
-C                                                                       
-C              TECTˆPOBAHˆE METO„OB ˆ‡ AKETA M                        
-C              METO„   ‹‹ˆCOˆ„OB     ( A9  )                          
-C                                                                       
-C        OˆCAHˆE APAMETPOB POƒPAMM› METO„A                           
-C                                                                       
-C      PA‡MEPHOCTœ MACCˆBOB X,A,B,G1  PABHA  N                          
-         REAL *8 X(2),A(2),B(2),G1(2)                                   
-         REAL *8 PAR(40),Y                                              
-         REAL *8 F,FNLP                                                 
-         EXTERNAL F,FNLP,GRAD,AGS                                       
-         INTEGER Q                                                      
-C                                                                       
-C           OˆCAHˆE O™ˆX O‹ACTE‰ METO„A                              
-C                                                                       
-         COMMON /A91/ BG/A92 / G /A93 / G2/A94 / GN                     
-     *          /A95 / XN /A96/ X1                                      
-C       PA‡MEPHOCTœ  MACCˆBA BG   = N*N                                 
-         REAL *8 BG  (2 ,2)                                             
-C       PA‡MEPHOCTœ  BCEX MACCˆBOB  = N                                 
-         REAL *8 G ( 2 ),G2( 2 ),GN( 2 ),XN( 2 ),X1(2)                  
-C                                                                       
-C           XAPAKTEPˆCTˆKA APAMETPOB POƒPAMM› METO„A                  
-C                                                                       
-C     N - PA‡MEPHOCTœ ‡A„A—ˆ                                            
-C     X - BEKTOP “PAB‹ŸEM›X EPEMEHH›X                                 
-C     A - BEKTOP ‹EB›X ƒPAHˆ– ˆ‡MEHEHˆŸ “PAB‹. EPEMEHH›X              
-C     B - BEKTOP PAB›X ƒPAHˆ– ˆ‡MEHEHˆŸ “PAB‹.EPEMEHH›X              
-C     F - ˆMŸ O„POƒPAMM› TˆA FUNCTION „‹Ÿ B›—ˆC‹EHˆŸ                 
-C         ‡HA—EHˆŸ KPˆTEPˆŸ                                             
-C     GRAD - ˆMŸ O„POƒPAMM› TˆA SUBROUTINE „‹Ÿ                       
-C         B›—ˆC‹EHˆŸ ƒPA„ˆEHTA                                          
-C     AGS - ˆMŸ O„POƒPAMM› TˆA SUBROUTINE „‹Ÿ PAC—ETA                
-C           MATPˆ–› BTOP›X POˆ‡BO„H›X (HE ˆCO‹œ‡“ETCŸ )               
-C     Y - ‡HA—EHˆE KPˆTEPˆŸ                                             
-C     G1 - BEKTOP ‡HA—EHˆ‰ POˆ‡BO„H›X ”“HK–ˆˆ F                        
-C     Q - APAMETP C ”ˆKCˆPOBAHH›M ‡HA—EHˆEM ( = 0)                     
-C     PAR - BEKTOP APAMETPOB METO„A                                    
-C     FNLP - ”ˆKCˆPOBAHHOE ˆMŸ O„POƒPAMM›                             
-C                                                                       
-C         ˆCXO„H›E „AHH›E ‡A„A—ˆ                                        
-C                                                                       
-C     PA‡MEPHOCTœ ‡A„A—ˆ                                                
-         N=2                                                            
-C     HA—A‹œHAŸ TO—KA                                                   
-         X(1)=  1.2                                                     
-         X(2)=  1.0                                                     
-C                                                                       
-C    ‡A„AHˆE APAMETPOB METO„A                                          
-C                                                                       
-         Q=0                                                            
-C     TO—HOCTœ PE˜EHˆŸ ‡A„A—ˆ O HOPME ƒPA„ˆEHTA                         
-         PAR(Q+1)=0.00001                                               
-C     MAKCˆMA‹œHOE —ˆC‹O ˜AƒOB,KOTOPOE MO†HO C„E‹ATœ                     
-         PAR(Q+2)=25                                                    
-C     B›XO„HO‰ APAMETP                                                 
-         PAR(Q+3)=0                                                     
-C     PA„ˆ“C HA—A‹œHOƒO ˜APA,OPE„E‹Ÿž™EƒO O‹ACTœ OˆCKA                
-         PAR(Q+4)=10.                                                   
-C    TO—HOCTœ PE˜EHˆŸ O„HOMEPHO‰ ‡A„A—ˆ B›OPA ˜AƒA                     
-         PAR(Q+5)=0.001                                                 
-C    MˆHˆMA‹œHO „O“CTˆM›‰ O'EM ‹‹ˆCOˆ„A                             
-         PAR(Q+6)=0.0000000000000001                                    
-C    HOMEP BEPCˆˆ METO„A ( =1 „‹Ÿ B›“K‹›X ”“HK–ˆ‰ ; =2, EC‹ˆ           
-C                           ”“HK–ˆŸ HEB›“K‹AŸ )                        
-         PAR(Q+7)=2                                                     
-C     OPŸ„OK „ˆ””EPEH–ˆPOBAHˆŸ ( = 1 ˆ‹ˆ 2 )                           
-         PAR(Q+8)=1                                                     
-C     ˜Aƒ „ˆ””EPEH–ˆPOBAHˆŸ                                              
-         PAR(Q+9)=0.00001                                               
-C     —ˆC‹O “„A—H›X ˜AƒOB,—EPE‡ KOTOPOE  C‹E„“ET B›BO„ˆTœ                
-C     ˆH”OPMA–ˆž                                                        
-         PAR(Q+10)=1                                                    
-C     CTEEHœ O„POHOCTˆ B›BO„ˆMO‰ ˆH”OPMA–ˆˆ ( OT 0 „O 3 )            
-         PAR(Q+11)=3                                                    
-C                                                                       
-         CALL A9 (N,X,A,B,F,GRAD,AGS,Y,G1,Q,PAR,FNLP)                   
-         END                                                            
-
+! TECTˆPOBAHˆE METO„OB ˆ‡ AKETA M METO„ ‹‹ˆCOˆ„OB (A9)
+PROGRAM VA9
+! XAPAKTEPˆCTˆKA APAMETPOB POƒPAMM› METO„A
+    ! N - PA‡MEPHOCTœ ‡A„A—ˆ
+    ! X - BEKTOP “PAB‹ŸEM›X EPEMEHH›X
+    ! A - BEKTOP ‹EB›X ƒPAHˆ– ˆ‡MEHEHˆŸ “PAB‹. EPEMEHH›X
+    ! B - BEKTOP PAB›X ƒPAHˆ– ˆ‡MEHEHˆŸ “PAB‹.EPEMEHH›X
+    ! F - ˆMŸ O„POƒPAMM› TˆA FUNCTION „‹Ÿ B›—ˆC‹EHˆŸ ‡HA—EHˆŸ KPˆTEPˆŸ
+    ! GRAD - ˆMŸ O„POƒPAMM› TˆA SUBROUTINE „‹Ÿ B›—ˆC‹EHˆŸ ƒPA„ˆEHTA
+    ! AGS - ˆMŸ O„POƒPAMM› TˆA SUBROUTINE „‹Ÿ PAC—ETA MATPˆ–› BTOP›X POˆ‡BO„H›X (HE ˆCO‹œ‡“ETCŸ )
+    ! Y - ‡HA—EHˆE KPˆTEPˆŸ
+    ! G1 - BEKTOP ‡HA—EHˆ‰ POˆ‡BO„H›X ”“HK–ˆˆ F
+    ! Q - APAMETP C ”ˆKCˆPOBAHH›M ‡HA—EHˆEM ( = 0)
+    ! PAR - BEKTOP APAMETPOB METO„A
+    ! FNLP - ”ˆKCˆPOBAHHOE ˆMŸ O„POƒPAMM›
+! OˆCAHˆE O™ˆX O‹ACTE‰ METO„A
+    ! OˆCAHˆE O™ˆX O‹ACTE‰ METO„A
+    COMMON /A91/ BG
+    COMMON /A92/ G
+    COMMON /A93/ G2
+    COMMON /A94/ GN
+    COMMON /A95/ XN 
+    COMMON /A96/ X1
+    ! PA‡MEPHOCTœ MACCˆBOB BCEX MACCˆBOB PABHA N
+    REAL(8),DIMENSION(2)::X,A,B,G1,G,G2,GN,XN,X1
+    ! PA‡MEPHOCTœ MACCˆBA BG = N*N
+    REAL(8),DIMENSION(2,2)::BG
+    REAL(8),DIMENSION(40)::PAR
+    REAL(8)::F,FNLP,Y
+    INTEGER::Q
+    EXTERNAL F,FNLP,GRAD,AGS
+! ˆCXO„H›E „AHH›E ‡A„A—ˆ
+    ! PA‡MEPHOCTœ ‡A„A—ˆ
+    N=2
+    ! HA—A‹œHAŸ TO—KA
+    X(1)=  1.2
+    X(2)=  1.0
+! ‡A„AHˆE APAMETPOB METO„A
+    Q=0
+    PAR(Q+1)=0.00001            ! TO—HOCTœ PE˜EHˆŸ ‡A„A—ˆ O HOPME ƒPA„ˆEHTA
+    PAR(Q+2)=25                 ! MAKCˆMA‹œHOE —ˆC‹O ˜AƒOB,KOTOPOE MO†HO C„E‹ATœ
+    PAR(Q+3)=0                  ! B›XO„HO‰ APAMETP
+    PAR(Q+4)=10.                ! PA„ˆ“C HA—A‹œHOƒO ˜APA,OPE„E‹Ÿž™EƒO O‹ACTœ OˆCKA
+    PAR(Q+5)=0.001              ! PE˜EHˆŸ O„HOMEPHO‰ ‡A„A—ˆ B›OPA ˜AƒA
+    PAR(Q+6)=0.0000000000000001 ! „O“CTˆM›‰ OšEM ‹‹ˆCOˆ„A
+    PAR(Q+7)=2                  ! HOMEP BEPCˆˆ METO„A ( =1 „‹Ÿ B›“K‹›X ”“HK–ˆ‰ ; =2, EC‹ˆ ”“HK–ˆŸ HEB›“K‹AŸ )
+    PAR(Q+8)=1                  ! OPŸ„OK „ˆ””EPEH–ˆPOBAHˆŸ ( = 1 ˆ‹ˆ 2 )
+    PAR(Q+9)=0.00001            ! ˜Aƒ „ˆ””EPEH–ˆPOBAHˆŸ
+    PAR(Q+10)=1                 ! —ˆC‹O “„A—H›X ˜AƒOB,—EPE‡ KOTOPOE ‘‹E„“ET B›BO„ˆTœ ˆH”OPMA–ˆž
+    PAR(Q+11)=3                 ! CTEEHœ O„POHOCTˆ B›BO„ˆMO‰ ˆH”OPMA–ˆˆ ( OT 0 „O 3 )
+! ‚›‡Ž‚ Œ…’Ž„€
+    CALL A9 (N,X,A,B,F,GRAD,AGS,Y,G1,Q,PAR,FNLP)
+END PROGRAM VA9
