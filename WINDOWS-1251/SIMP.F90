a4076f6e52030633bc69498b3046f56dd8cf2211
msem <50719590+msemina@users.noreply.github.com>
2020-03-16 02:23:05 +0300

    Актуальная версия

--- WINDOWS-1251/SIMP.F90
+++ WINDOWS-1251/SIMP.F90
@@ -1,4 +1,3 @@
-! ÏOÄÏPOÃPAMMA CÈMÏËEKC-METOÄA ÄËß PEØEHÈß BCÏOMOÃATEËÜHÛX ÇAÄA× B METOÄAX HËÏ
 SUBROUTINE SIMP(X1,M1,N1,L1,F1)
 ! ÎÏÈÑÀÍÈÅ ÏÅÐÅÌÅÍÍÛÕ
     COMMON /A20/ SM
@@ -14,9 +13,8 @@ SUBROUTINE SIMP(X1,M1,N1,L1,F1)
     REAL(8),DIMENSION(3)::U1
     REAL(8),DIMENSION(4)::Y1
     REAL(8),DIMENSION(N3,N4)::X1
-    ! ÑËÓÆÅÁÍÛÅ ÏÅÐÅÌÅÍÍÛÅ
-    INTEGER::SP_VAR
-! ÂÛ×ÈÑËßÅÌÀß ×ÀÑÒÜ
+    INTEGER::SP_VAR ! ÑËÓÆÅÁÍÀß ÏÅÐÅÌÅÍÍÀß
+! ÊÎÄ ÏÎÄÏÐÎÃÐÀÌÌÛ
     N2=N1+M+1
     DO J=1,N1
         SM(J)=J
@@ -27,7 +25,7 @@ SUBROUTINE SIMP(X1,M1,N1,L1,F1)
     END DO
     EP=DABS(X1(1,2))
     DO I=1,N1
-        IF(DABS(X1(1,I+1))>EP) EP=DABS(X1(1,I+1))
+        IF(DABS(X1(1,I+1))>EP)EP=DABS(X1(1,I+1))
     END DO
     EP=100*EP
     N11=N1+1
@@ -48,9 +46,9 @@ SUBROUTINE SIMP(X1,M1,N1,L1,F1)
     EP=1.D-5
     D1=0
     V=0
-    SP_VAR=0
+    SP_VAR=30
     DO
-        IF(SP_VAR==0)THEN
+        IF(SP_VAR==30)THEN
             Z=-EP
             R=0
             DO I=1,M1
@@ -59,7 +57,9 @@ SUBROUTINE SIMP(X1,M1,N1,L1,F1)
                     Z=X1(I+1,1)
                 END IF
             END DO
-            IF(R/=0)THEN
+            IF(R==0)THEN
+                SP_VAR=40
+            ELSE
                 D1=-1
                 TH=1.D+16
                 K=0
@@ -74,15 +74,13 @@ SUBROUTINE SIMP(X1,M1,N1,L1,F1)
                 END DO
                 IF(K==0)THEN
                     SP_VAR=60
-                    EXIT
-                    CYCLE
                 ELSE
                     SP_VAR=50
                 END IF
             END IF
         END IF
         !
-        IF((SP_VAR==0).OR.(SP_VAR==40))THEN
+        IF(SP_VAR==40)THEN
             Z=-EP
             K=0
             DO J=1,N1
@@ -92,30 +90,29 @@ SUBROUTINE SIMP(X1,M1,N1,L1,F1)
                 END IF
             END DO
             IF(K==0)THEN
-                SP_VAR=0
-                EXIT
-                CYCLE
-            END IF
-            TH=1.D+16
-            R=0
-            DO I=1,M1
-                IF(X1(I+1,K+1)>EP)THEN
-                    Z=X1(I+1,1)/X1(I+1,K+1)
-                    IF(Z<TH)THEN
-                        TH=Z
-                        R=I
+                SP_VAR=70
+            ELSE
+                TH=1.D+16
+                R=0
+                DO I=1,M1
+                    IF(X1(I+1,K+1)>EP)THEN
+                        Z=X1(I+1,1)/X1(I+1,K+1)
+                        IF(Z<TH)THEN
+                            TH=Z
+                            R=I
+                        END IF
                     END IF
+                END DO
+                IF(R==0)THEN
+                    SP_VAR=60
+                ELSE
+                    D1=1
+                    SP_VAR=50
                 END IF
-            END DO
-            IF(R==0)THEN
-                SP_VAR=60
-                EXIT
-                CYCLE
             END IF
-            D1=1
         END IF
         !
-        IF((SP_VAR==0).OR.(SP_VAR==40).OR.(SP_VAR==50))THEN
+        IF(SP_VAR==50)THEN
             Z=X1(R+1,K+1)
             N11=N1+1
             DO J=1,N11
@@ -124,50 +121,58 @@ SUBROUTINE SIMP(X1,M1,N1,L1,F1)
                         TH=X1(R+1,J)/Z
                         M11=M1+1
                         DO I=1,M11
-                            IF(I/=R+1)THEN
-                                X1(I,J)=X1(I,J)-X1(I,K+1)*TH
-                            END IF
+                            IF(I/=R+1)X1(I,J)=X1(I,J)-X1(I,K+1)*TH
                         END DO
                     END IF
                 END IF
             END DO
-        END IF
-        !
-        V=V+1
-        I=SM(K)
-        SM(K)=SN(R)
-        SN(R)=I
-        N11=N1+1
-        DO J=1,N11
-            X1(R+1,J)=X1(R+1,J)/Z
-        END DO
-        M11=M1+1
-        DO I=1,M11
-            X1(I,K+1)=-X1(I,K+1)/Z
-        END DO
-        X1(R+1,K+1)=1/Z
-        !
-        IF(D1>0)SP_VAR=40
-    END DO
-    !
-    IF(SP_VAR==60)X1(1,1)=1.D+15
-    F1=X1(1,1)
-    IF(X1(1,1)<0.999*1.D+15)THEN
-        DO J=1,M1
-            IF(SN(J)<=N1) U1(SN(J))=X1(J+1,1)
-        END DO
-        DO J=1,N1
-            IF(SM(J)>N1)THEN
-                I=SM(J)-N1
-                Y1(I)=Y1(I)-X1(1,J+1)
+            V=V+1
+            I=SM(K)
+            SM(K)=SN(R)
+            SN(R)=I
+            N11=N1+1
+            DO J=1,N11
+                X1(R+1,J)=X1(R+1,J)/Z
+            END DO
+            M11=M1+1
+            DO I=1,M11
+                X1(I,K+1)=-X1(I,K+1)/Z
+            END DO
+            X1(R+1,K+1)=1/Z
+            IF(D1>0)THEN
+                SP_VAR=40
+            ELSE
+                SP_VAR=30
             END IF
-        END DO
-        DO I=1,N1
-            X1(1,I+1)=U1(I)
-        END DO
-        DO I=1,M1
-            X1(I+1,1)=Y1(I)
-        END DO
-    END IF
-    RETURN
+        END IF
+        ! 
+        IF(SP_VAR==60)THEN
+            X1(1,1)=1.D+15
+            SP_VAR=70
+        END IF
+        ! 
+        IF(SP_VAR==70)THEN
+            F1=X1(1,1)
+            IF(X1(1,1)<0.999*1.D+15)THEN
+                DO J=1,M1
+                    IF(SN(J)<=N1) U1(SN(J))=X1(J+1,1)
+                END DO
+                DO J=1,N1
+                    IF(SM(J)>N1)THEN
+                        I=SM(J)-N1
+                        Y1(I)=Y1(I)-X1(1,J+1)
+                    END IF
+                END DO
+                DO I=1,N1
+                    X1(1,I+1)=U1(I)
+                END DO
+                DO I=1,M1
+                    X1(I+1,1)=Y1(I)
+                END DO
+            END IF
+            SP_VAR=80
+        END IF
+        ! 
+        IF(SP_VAR==80)RETURN
+    END DO
 END SUBROUTINE SIMP
